3161dcc236935173d92473ea611fcb77dbdc1bce
Calouche, Philip <philip.calouche@outlook.com>
2017-10-09 00:15:43 -0400

    removed non-working angular code for now, fix pom so WAR option works as expected

--- spat-ui/src/app/app.module.ts
+++ spat-ui/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TeamManagerComponent } from './components/team-manager/team-manager.component';
-import { UserManagerComponent } from './components/user-manager/user-manager.component';
-import { TeamManagerModalComponent } from './components/team-manager/team-manager-modal.component';
+import {AppComponent} from './app.component';
+import {TeamManagerComponent} from './components/team-manager/team-manager.component';
+import {UserManagerComponent} from './components/user-manager/user-manager.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamManagerComponent,
-    UserManagerComponent,
-    TeamManagerModalComponent
+    UserManagerComponent
   ],
   imports: [
     BrowserModule
@@ -19,4 +17,5 @@ import { TeamManagerModalComponent } from './components/team-manager/team-manage
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
