beebd880f4c4259772ded34469356acd3099f727
Calouche, Philip <philip.calouche@outlook.com>
2018-01-28 14:27:17 -0500

    moved config for static resources to correct project

---
+++ ui/src/main/java/com/pcalouche/spat/ui/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.pcalouche.spat.ui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**")
+      .addResourceLocations("classpath:/public/")
+      .setCachePeriod(3600)
+      .resourceChain(true)
+      .addResolver(new PathResourceResolver() {
+        @Override
+        protected Resource getResource(String resourcePath,
+                                       Resource location) throws IOException {
+          Resource requestedResource = location.createRelative(resourcePath);
+          return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+            : new ClassPathResource("/public/index.html");
+        }
+      });
+  }
+}
