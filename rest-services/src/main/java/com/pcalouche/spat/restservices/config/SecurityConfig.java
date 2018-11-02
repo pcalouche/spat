847d8db9674f3504d7d26b401452f5662e883941
Philip Calouche <philip.calouche@tma1.com>
2018-11-02 17:01:47 -0400

    tweak security config more

--- rest-services/src/main/java/com/pcalouche/spat/restservices/config/SecurityConfig.java
+++ rest-services/src/main/java/com/pcalouche/spat/restservices/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.pcalouche.spat.restservices.security.filter.JwtAuthenticationProcessi
 import com.pcalouche.spat.restservices.security.provider.AjaxLoginAuthenticationProvider;
 import com.pcalouche.spat.restservices.security.provider.JwtAuthenticationProvider;
 import com.pcalouche.spat.restservices.security.util.SecurityUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
@@ -23,20 +24,32 @@ import java.util.Arrays;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserRepository userRepository;
     private final ObjectMapper objectMapper;
+    private final AjaxLoginAuthenticationProvider ajaxLoginAuthenticationProvider;
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final AuthenticationManager authenticationManager;
 
     public SecurityConfig(UserRepository userRepository,
                           ObjectMapper objectMapper) {
-        this.userRepository = userRepository;
         this.objectMapper = objectMapper;
+        ajaxLoginAuthenticationProvider = new AjaxLoginAuthenticationProvider(userRepository);
+        jwtAuthenticationProvider = new JwtAuthenticationProvider(userRepository);
+        authenticationManager = new ProviderManager(Arrays.asList(ajaxLoginAuthenticationProvider, jwtAuthenticationProvider));
+    }
+
+    @Override
+    public AuthenticationManager authenticationManager() {
+        return authenticationManager;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        AjaxLoginAuthenticationProvider ajaxLoginAuthenticationProvider = new AjaxLoginAuthenticationProvider(userRepository);
-        JwtAuthenticationProvider jwtAuthenticationProvider = new JwtAuthenticationProvider(userRepository);
-        AuthenticationManager authenticationManager = new ProviderManager(Arrays.asList(ajaxLoginAuthenticationProvider, jwtAuthenticationProvider));
         AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter(authenticationManager, objectMapper);
         JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter = new JwtAuthenticationProcessingFilter(authenticationManager, objectMapper);
 
@@ -60,6 +73,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SecurityUtils.AUTHENTICATED_PATH).authenticated()
                 // Setup filters for the endpoints
                 .and()
+                .authenticationProvider(ajaxLoginAuthenticationProvider)
+                .authenticationProvider(jwtAuthenticationProvider)
                 .addFilterBefore(ajaxLoginProcessingFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationProcessingFilter, UsernamePasswordAuthenticationFilter.class);
     }
