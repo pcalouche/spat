49f0f481803386db3bc86cae0a35716ec1369b11
Philip Calouche <philip.calouche@tma1.com>
2018-12-13 09:32:21 -0500

    unregister service worker

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,19 +1,23 @@
-import React                 from 'react';
-import ReactDOM              from 'react-dom';
-import {Provider}            from 'react-redux';
-import {BrowserRouter}       from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
+import React              from 'react';
+import ReactDOM           from 'react-dom';
+import {Provider}         from 'react-redux';
+import {BrowserRouter}    from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 import 'react-app-polyfill/ie11';
 
 import './index.scss';
-import store                 from './redux/store';
-import App                   from './App';
+import store              from './redux/store';
+import App                from './App';
 
 ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter basename="/spat/ui/react">
-        <App/>
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={store}>
+    <BrowserRouter basename="/spat/ui/react">
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
